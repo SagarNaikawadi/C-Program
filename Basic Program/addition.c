6c408871761834e5e19ec570affa34369a5fb5e3
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-08-03 12:08:32 +0530

    Add files via upload

---
+++ Basic Program/addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+
+{   int a,b,c;
+  printf("enter any two number");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("sum of two number");
+  
+  return 0;
+}
