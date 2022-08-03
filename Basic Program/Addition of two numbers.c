ac656dafa9e4151e73738efbf127fbcdaad0121d
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-08-03 12:13:33 +0530

    Add files via upload

---
+++ Basic Program/Addition of two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+
+{   int a,b,Sum;
+  printf("\n Enter any two number = ");
+  scanf("%d%d",&a,&b);
+  Sum=a+b;
+  printf("sum of two number = %d",Sum);
+  
+  return 0;
+}
