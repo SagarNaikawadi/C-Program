0e54082bea3f9435e5d92a1c0c6a7b0ddcca59b7
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-08-07 20:18:31 +0530

    Add files via upload

---
+++ Pointer programs/05 pointer Arithmetic  Decrement by integral constant.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int Num[5]={21,38,54,48,51};
+    int *ptr=&Num[3];
+    
+    printf("\n Base Address of Array =%d",Num);
+    printf("\n value of pointer is = %d",ptr);
+    printf("\n value of Array Elements where pointer pointing %d",*ptr);
+    
+    getch();
+    ptr = ptr - 3;
+    
+    printf("\n\n New value in pointer is %d",ptr);
+    printf("\n Value of Array Element where pointer pointing %d",*ptr);
+    
+    getch();
+    return 0;
+    
+}
