df30a1af03054e47e4a24d5f2e68bc6633a2ba4d
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-14 19:04:14 +0530

    Add files via upload

---
+++ Pattern programs/10 Right Upper Triangular pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{ 
+    int r = 0,c = 0,cnt = 0;
+    
+    printf("\n Enter Rows and columns Count");
+    scanf("%d",&cnt);
+    
+    for(r=1;r<=cnt;r++)
+    {
+      for(c=1;c<=cnt;c++)
+      {
+        if(r<=c)
+        {
+          printf(" * ");
+        }
+        else 
+        {
+          printf("   ");
+        }
+      }
+       printf("\n");
+    }
+    getch();
+    return 0;
+}
