83b18c3e8965ecda49e17e8569f2e67d0e372a0c
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-15 23:32:27 +0530

    Add files via upload

---
+++ Digit Program/05 Program to calculate zero Digit count in goven number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int No = 0,Dig = 0,Zcnt = 0,Temp = 0;
+    Up:
+      
+    printf("\n Enter Any Positive Integer Number =>");
+    scanf("%d",&No);
+    Temp = No;
+    
+    if(No < 0)
+    {
+      printf("\n Invalid Input");
+      goto Up;
+    }
+    while (Temp > 0)
+    {
+      Dig = Temp % 10;
+      if(Dig  == 0)
+      {
+        Zcnt++;
+      }
+      Temp = Temp / 10;
+    }
+    printf("\n Even count in given number %d is = %d",No,Zcnt);
+    
+    getch();
+    return 0;
+}
