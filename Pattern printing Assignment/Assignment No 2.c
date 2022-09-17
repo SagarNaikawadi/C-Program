65c704eea587bc49492d61eabfbfed567f38a700
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-17 23:49:37 +0530

    Add files via upload

---
+++ Pattern printing Assignment/Assignment No 2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{ 
+    int r = 0,c = 0,cnt = 0;
+    char Ch = 'A';
+    
+    printf("\n Enter Rows and columns Count =>");
+    scanf("%d",&cnt);
+    
+    for(r = 1;r <= cnt;r++)
+    {
+      for(c = 1;c <= cnt;c++)
+      {
+        if(r >= c)
+        {
+          printf("%c",Ch);  
+        }   
+        else 
+        {
+          printf("   "); 
+        }
+        Ch++;
+      }
+      printf("\n");
+    }
+    getch();
+    return 0;
+}
