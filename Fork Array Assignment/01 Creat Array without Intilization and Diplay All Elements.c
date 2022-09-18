ac8379c6e27a902d73ed6b562d044cda0447d023
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-18 23:26:22 +0530

    Add files via upload

---
+++ Fork Array Assignment/01 Creat Array without Intilization and Diplay All Elements.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int Arr[5] = { };
+    
+    printf("\n Value of 1st element is %d ",Arr[0]);
+    printf("\n Value of 2nd element is %d ",Arr[1]);
+    printf("\n Value of 3rd element is %d ",Arr[2]);
+    printf("\n Value of 4th element is %d ",Arr[3]);
+    printf("\n Value of 5th element is %d ",Arr[4]);
+    
+    getch();
+    return 0;
+}
