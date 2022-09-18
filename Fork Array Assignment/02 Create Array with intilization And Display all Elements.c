46fa24c0180c08f0d5268b6076a85903943907a7
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-18 23:35:35 +0530

    Add files via upload

---
+++ Fork Array Assignment/02 Create Array with intilization And Display all Elements.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int Arr[6] = {53,67,87,43,98,34};
+    
+    printf("\n Value of 1st element is %d",Arr[0]);
+    printf("\n value of 2nd element is %d",Arr[1]);
+    printf("\n Value of 3rd element is %d",Arr[2]);
+    printf("\n value of 4th element is %d",Arr[3]);
+    printf("\n Value of 5th element is %d",Arr[4]);
+    printf("\n value of 6th element is %d",Arr[5]);
+  
+    getch();
+    return 0;
+}
