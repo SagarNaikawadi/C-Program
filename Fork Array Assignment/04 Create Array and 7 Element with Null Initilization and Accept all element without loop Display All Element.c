640ad48601e23f5e831fe7d4818be291cdbcc469
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-20 23:34:53 +0530

    Add files via upload

---
+++ Fork Array Assignment/04 Create Array and 7 Element with Null Initilization and Accept all element without loop Display All Element.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int Arr[7] = { };
+  
+  printf("\n Value of 1st Element =>");;
+  scanf("%d",&Arr[0]);
+  
+  printf("\n Value of 2nd Element =>");;
+  scanf("%d",&Arr[1]);
+  
+  printf("\n Value of 3rd  Element =>");;
+  scanf("%d",&Arr[2]);
+  
+  printf("\n Value of 4th Element =>");;
+  scanf("%d",&Arr[3]);
+  
+  printf("\n Value of 5th Element =>");;
+  scanf("%d",&Arr[4]);
+  
+  printf("\n Value of 6th Element =>");;
+  scanf("%d",&Arr[5]); 
+  
+  printf("\n Value of 7th Element =>");;
+  scanf("%d",&Arr[6]); 
+  
+  printf("\n Value of 101 Elemnt is = %d",Arr[0]);
+  printf("\n Value of 102 Elemnt is = %d",Arr[1]);
+  printf("\n Value of 103 Elemnt is = %d",Arr[2]);
+  printf("\n Value of 104 Elemnt is = %d",Arr[3]);
+  printf("\n Value of 105 Elemnt is = %d",Arr[4]);
+  printf("\n Value of 106 Elemnt is = %d",Arr[5]);
+  printf("\n Value of 107 Elemnt is = %d",Arr[6]);
+  
+  getch();
+  return 0;
+}
