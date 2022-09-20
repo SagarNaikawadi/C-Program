0c783987d54276322265f867647356af0d43235f
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-20 23:34:16 +0530

    Add files via upload

---
+++ Fork Array Assignment/03 Create Array and 7 Element with Null Initilization and Display All Element.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int Arr[7] = { };
+    
+    printf("\n Value of 1st Element is = %d",Arr[0]);
+    printf("\n Value of 2nd Element is = %d",Arr[1]);
+    printf("\n Value of 3rd Element is = %d",Arr[2]);
+    printf("\n Value of 4th Element is = %d",Arr[3]);
+    printf("\n Value of 5th Element is = %d",Arr[5]);
+    printf("\n Value of 6th Element is = %d",Arr[6]);
+    
+    getch();
+    return 0;
+}
