3c3a8e3f8d5b53fa401c304b135089a3917c9616
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-09-29 19:34:44 +0530

    Add files via upload

---
+++ Fork Array Assignment/06B Accept Array Element and Search given number on index.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    int Arr[5] = { },i = 0,src = 0;
+    
+    for(i = 0;i < 5;i++)
+    {
+      printf("\n Enter Array Element %d => ",i+1);
+      scanf("%d",&Arr[i]);
+    }
+    printf("\n Enter Seraching Element ");
+    scanf("%d",&src);
+  
+    for(i = 0;i<5;i++)
+    {
+      if(Arr[i]==src)
+      {
+        break;
+      }
+    }
+    if(i<5)
+    {
+      printf("\n Enter Element found on index %d",i);
+    }
+    else
+    {
+      printf("\n Enter Elemnt Not found");
+    }
+    
+    getch();
+    return 0;
+}
