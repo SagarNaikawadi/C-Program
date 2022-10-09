1f1f90d8789ae92e8a231a73be930c785714e17e
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-10-10 00:07:26 +0530

    Add files via upload

---
+++ String Programs/01 Puts Gets Demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<comcat.h>
+
+int main()
+{
+    char cArr[50] ={'\0'};
+
+    puts("\n Enter The String => ");
+    gets(cArr);
+
+    puts("\n Given String is ");
+    puts(cArr);
+
+    getch();
+    return 0;
+}
