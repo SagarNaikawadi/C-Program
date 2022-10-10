0a4a2ccc074e3d6897eefd36e285a88a05adfafc
SagarNaikawadi <109465416+SagarNaikawadi@users.noreply.github.com>
2022-10-10 22:03:24 +0530

    Add files via upload

---
+++ String Programs/04 Captial Letters count in given string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    char cSrc[40] = {'\0'};
+    int i = 0,Scnt = 0;
+    
+    
+    puts("\n Enter The String => ");
+    gets(cSrc);
+    
+    while(cSrc[i] != '\0')
+    {
+      if(cSrc[i] >='A' && cSrc[i] <= 'Z')
+      {
+        Scnt++;
+      }
+      i++;
+    }
+    
+    printf("\n Small Letters Count in Given String is %d",Scnt);
+    
+    getch();
+    return 0;
+}
